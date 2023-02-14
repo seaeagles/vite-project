27e51fcf86001e69eeb57d313570751b247ae22e
David M <dminerur@outlook.com>
2023-02-14 12:54:00 -0500

    added dragon cards and styles per lecture code-along

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,19 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
 
+
+function DragonCard(props) {
+  console.log(props)
+  return (
+    <div className={props.type + ' dragon-card'}>
+      <h1>{props.type}</h1>
+      <div>{props.icon} <span>{props.clicks}</span></div>
+    </div>
+  )
+}
+
+
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div className="App">
@@ -15,18 +26,10 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Dave M</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <DragonCard clicks={0} type={'fire'} icon={'🐉'}/>
+      <DragonCard clicks={0} type={'water'} icon={'🐉'}/>
+      <DragonCard clicks={0} type={'air'} icon={'🐉'}/>
+      <DragonCard clicks={0} type={'earth'} icon={'🐉'}/>
     </div>
   )
 }
